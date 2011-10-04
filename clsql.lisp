a7bca31fd4a0ac5fa4a06debb4b3bfba6b150d90
Russ Tyndall <russ@acceleration.net>
2011-10-04 15:29:59 -0400

    worked on serial import from csv to make it more robust and more prone to actually make it through a large import

--- clsql.lisp
+++ clsql.lisp
@@ -1,6 +1,9 @@
 (in-package :cl-csv)
 (cl-interpol:enable-interpol-syntax)
 
+(defun exec (sql)
+  (clsql-sys:execute-command sql))
+
 (defmethod format-csv-value ((val clsql-sys:date))
   (clsql-helper:print-nullable-date val))
 
@@ -41,23 +44,45 @@
     (apply #'data-table:ensure-table-for-data-table dt table-name keys)
     (data-table:import-data-table dt table-name excluded-columns :row-fn row-fn)))
 
-(defun serial-import-from-csv (table-name &key file
-                               (schema "public") (downcase-columns T)
-                               (progress-stream T) (progress-mod 5000))
-  "Will make a best effor to create a table matching the csv's schema and then "
-  (let ((cnt 0) columns)
-    (flet ((row-fn (row)
-             (if (= cnt 0)
-                 (setf columns
-                       (mapcar
-                        (lambda (x) (clsql-helper::trim-and-nullify
-                                (if downcase-columns (nstring-downcase x) x)))
-                        row))
-                 (clsql-sys:insert-records
-                  :into (clsql-sys:sql-expression :string #?"${schema}.${table-name}")
-                  :attributes columns
-                  :values row))
+(defun serial-import-from-csv (table-name
+                               &key file
+                               (schema "public") (column-transform
+                                                  #'data-table::english->postgres)
+                               (progress-stream T) (progress-mod 5000)
+                               (log (lambda (msg &rest args)
+                                      (apply #'format progress-stream msg args)))
+                               (on-error 'continue-importing)
+                               &aux (cnt 0) columns
+                               (cl-interpol:*list-delimiter* ", "))
+  "Will make a best effor to create a table matching the csv's schema and then
+
+   progress-stream: the default log stream to print to
+   progress-mod: how often we should report progress (in number of rows)
+   log: is a function that accepts msg and args to display status updates to the user
+   on-error: is a restart to run when an error is experienced, if nil, it will simply
+            allow the error to go up the stack.  the default is to continue with the
+            import, skipping the row that caused errors
+  "
+  (labels ((handled-insert (sql log row)
+             (restart-case
+                 (handler-bind
+                     ((error (lambda (c)
+                               (funcall log "Error importing ROW ~D of file: ~S~%~S~%~A~%~S"
+                                        cnt file row c c)
+                               (when on-error
+                                 (when (find-restart on-error) (invoke-restart on-error))))))
+                   (exec sql))
+               (continue-importing ()
+                 :report "Continue Importing the file, skipping this row of data")))
+           (row-fn (row &aux data sql)
              (incf cnt)
+             (when (= cnt 1)
+               (setf columns
+                     (data-table::sql-escaped-column-names row :transform column-transform))
+               (return-from row-fn))
+             (setf data (mapcar #'clsql-helper:format-value-for-database row)
+                   sql #?"INSERT INTO ${schema}.${table-name} (@{ columns }) VALUES ( @{data} )")
+             (handled-insert sql log row)
              (when (zerop (mod cnt progress-mod))
-               (format progress-stream "Imported ~D rows~%" cnt ))))
-      (read-csv file :row-fn #'row-fn))))
+               (funcall log "Imported ~D rows~%" cnt ))))
+    (read-csv file :row-fn #'row-fn)))
