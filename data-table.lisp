9f692e7ee9158e3f5b8747a1f063bc43bc4c367f
Ryan Davis <ryan@acceleration.net>
2011-10-21 13:54:55 -0400

    allow specifying a sample size on get-data-table-from-csv

--- data-table.lisp
+++ data-table.lisp
@@ -1,16 +1,14 @@
 (in-package :cl-csv)
 (cl-interpol:enable-interpol-syntax)
 
-(defun get-data-table-from-csv (file &optional (has-column-names t) (munge-types T))
-  (let* ((dt (make-instance 'data-table:data-table))
-         (data (read-csv
-                file
-                :map-fn (lambda (row) (mapcar #'data-table::trim-and-nullify row)))))
-    (if has-column-names
-        (setf
-         (data-table:column-names dt) (first data)
-         (data-table:rows dt) (rest data))
-        (setf (data-table:rows dt) data))
-    (when munge-types
-      (data-table:coerce-data-table-of-strings-to-types dt))
-    dt))
+(defun get-data-table-from-csv (file &optional (has-column-names t) (munge-types T) sample
+                                &aux (dt (make-instance 'data-table:data-table)))
+  (with-csv-input-stream (in-stream file)
+    (flet ((map-fn (row) (mapcar #'data-table::trim-and-nullify row)))
+      (when has-column-names
+        (setf (data-table:column-names dt) (map-fn (read-csv-row in-stream))))
+      (setf (data-table:rows dt)
+            (read-csv in-stream :map-fn #'map-fn :sample sample))
+      (when munge-types
+        (data-table:coerce-data-table-of-strings-to-types dt))
+      dt)))
