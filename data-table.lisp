9f0e55c3e37d5de202f6065bc783fc7d58f91507
Cyrus Harmon <ch-github@bobobeach.com>
2012-06-12 13:54:35 -0700

    added cl-csv-data-table.asd, new package and function  * added get-data-from-csv-list  * use cl-csv:: for unexported cl-csv symbols

--- data-table.lisp
+++ data-table.lisp
@@ -1,14 +1,36 @@
-(in-package :cl-csv)
-(cl-interpol:enable-interpol-syntax)
+
+(cl:defpackage #:cl-csv-data-table
+  (:use :cl)
+  (:use :cl-csv)
+  (:export #:get-data-table-from-csv
+           #:get-data-table-from-list))
+
+(cl:in-package #:cl-csv-data-table)
 
 (defun get-data-table-from-csv (file &optional (has-column-names t) (munge-types T) sample
                                 &aux (dt (make-instance 'data-table:data-table)))
-  (with-csv-input-stream (in-stream file)
+  (cl-csv::with-csv-input-stream (in-stream file)
     (flet ((map-fn (row) (mapcar #'data-table::trim-and-nullify row)))
       (when has-column-names
-        (setf (data-table:column-names dt) (map-fn (read-csv-row in-stream))))
+        (setf (data-table:column-names dt) (map-fn (cl-csv::read-csv-row in-stream))))
       (setf (data-table:rows dt)
             (read-csv in-stream :map-fn #'map-fn :sample sample))
       (when munge-types
         (data-table:coerce-data-table-of-strings-to-types dt))
       dt)))
+
+(defun get-data-table-from-csv-list
+    (list &optional (has-column-names t) (munge-types T) sample
+          &aux (dt (make-instance 'data-table:data-table)))
+  (flet ((map-fn (row) (mapcar #'data-table::trim-and-nullify row)))
+    (when has-column-names
+      (setf (data-table:column-names dt) (map-fn (first list))))
+    (setf (data-table:rows dt)
+          (mapcar (lambda (x)
+                    (if sample
+                        (map-fn (subseq x 0 sample))
+                        (map-fn x)))
+                  (rest list)))
+    (when munge-types
+      (data-table:coerce-data-table-of-strings-to-types dt))
+    dt))
