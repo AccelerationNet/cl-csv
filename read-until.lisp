38a0ffd5e21238b2fbfb4793c74fb738bba4b36c
Russ Tyndall <russ@acceleration.net>
2014-01-16 16:04:10 -0500

    tried copying SBCLs "fast" implentation of readline (which is a branch inside of READLINE)
    This seems to be using rewindable streams and buffers to make readline
    faster than read until, I was hoping that by using this implementation
    I could see the same performance, but for whatever reason this seems
    twice as slow :/
    
    re #13
    
    Programming:#310 (1.25)

--- read-until.lisp
+++ read-until.lisp
@@ -36,3 +36,89 @@
         (return-from read-into-buffer-until
           (values (- i nl-idx) t))))
     buffer-len))
+
+(defun fast-read-into-buffer-until (buffer stream &optional (nl #\newline))
+  #+sbcl
+  (return-from fast-read-into-buffer-until
+    (if (sb-impl::ansi-stream-cin-buffer stream)
+        (%fast-read-into-buffer-until buffer stream)
+        (read-into-buffer-until buffer stream nl)))
+  #-sbcl
+  (read-into-buffer-until buffer stream nl))
+
+#+sbcl ;; copied and mutated from sb-impl::ansi-stream-read-line-from-frc-buffer
+(defun %fast-read-into-buffer-until (buffer stream &optional (nl #\newline))
+  (sb-int:prepare-for-fast-read-char stream
+    (declare (ignore sb-impl::%frc-method%))
+    (let ((b-len (length buffer)) (b-idx 0))
+      (labels ((refill-buffer ()
+                 (prog1
+                     (sb-int:fast-read-char-refill stream nil nil)
+                   (setf sb-impl::%frc-index%
+                         (sb-kernel:ansi-stream-in-index sb-impl::%frc-stream%))))
+               (newline-position ()
+                 (etypecase nl
+                   (string (search nl (the (simple-array character (*))
+                                           sb-impl::%frc-buffer%)
+                                   :test #'char= :start2 sb-impl::%frc-index%))
+                   (character (position nl (the (simple-array character (*))
+                                                sb-impl::%frc-buffer%)
+                                        :test #'char=
+                                        :start sb-impl::%frc-index%))))
+               (return-partial-buffer (pos &optional nl?)
+                 (replace buffer sb-impl::%frc-buffer%
+                          :start1 b-idx
+                          :start2 sb-impl::%frc-index%
+                          :end2 pos)
+                 (setf sb-impl::%frc-index% (1+ pos))
+                 (sb-int:done-with-fast-read-char)
+                 (return-from %fast-read-into-buffer-until
+                   (values (+ 1 b-idx) nl? )))
+
+               (return-full-buffer (&aux (remaining (- b-len b-idx))
+                                    (end (+ sb-impl::%frc-index% remaining))
+                                    (end-l (1+ end)))
+                 
+                 (replace buffer sb-impl::%frc-buffer%
+                          :start1 b-idx
+                          :start2 sb-impl::%frc-index%
+                          :end2 end)
+                 (setf sb-impl::%frc-index% end-l)
+                 (sb-int:done-with-fast-read-char)
+                 (return-from %fast-read-into-buffer-until b-len))
+
+               (copy-into-buffer ()
+                 (let* ((end (length sb-impl::%frc-buffer%))
+                        (len (- end sb-impl::%frc-index%)))
+                   (replace buffer sb-impl::%frc-buffer%
+                            :start1 b-idx
+                            :start2 sb-impl::%frc-index%
+                            :end2 end)
+                   (setf b-idx (+ b-idx len))
+                   (when (refill-buffer)
+                     (return-from %fast-read-into-buffer-until
+                       (+ 1 b-idx))))))
+        
+        (declare (inline return-full-buffer return-partial-buffer copy-into-buffer refill-buffer))
+        (when (and (= sb-impl::%frc-index% sb-impl::+ansi-stream-in-buffer-length+)
+                   (refill-buffer))
+          ;; EOF had been reached before we read anything
+          ;; at all. Return the EOF value or signal the error.
+          (sb-int:done-with-fast-read-char)
+          (error 'end-of-file :stream stream))
+        (loop do
+          (let* ((pos (newline-position))
+                 (line-len (and pos (- pos sb-impl::%frc-index%)))
+                 (end (length sb-impl::%frc-buffer%))
+                 (len (- end sb-impl::%frc-index%))
+                 (new-out-len (+ b-idx len)))
+            ;;(when pos (adwutils:spy-break pos new-out-len b-idx buffer))
+            (cond
+              ;; line fits in our buffer and we have our new line
+              ((and pos (<= (+ b-idx line-len) b-len))
+               (return-partial-buffer pos t))
+
+              ((< b-len new-out-len)
+               (return-full-buffer))
+
+              (T (copy-into-buffer)))))))))
